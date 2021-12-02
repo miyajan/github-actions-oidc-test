a1a1cbe303b0e635c6b16a9ff4fdc4d0dccaeee7
jumpei-miyata <jumpei-miyata@cybozu.co.jp>
2021-12-02 21:54:03 +0900

    First commit

---
+++ terraform/iam.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
+}
+
+resource "aws_iam_role" "github_actions_oidc_test" {
+  name = "GitHubActionsOIDCTest"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : aws_iam_openid_connect_provider.github_actions.arn
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:sub" : "repo:miyajan/github-actions-oidc-test:ref:refs/heads/main"
+          },
+        }
+      }
+    ]
+  })
+}
---
+++ terraform/terraform.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = "1.0.10"
+
+  required_providers {
+    aws = {
+      version = "3.63.0"
+    }
+  }
+}
